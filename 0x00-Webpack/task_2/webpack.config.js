8d51561035036a3a2c90aee639e412d9f16ab3ec
Solomon Nwante <myphilothea@gmail.com>
2024-10-28 23:47:40 +0100

    Setup webpack config file

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Rule for CSS files
+        use: ['style-loader', 'css-loader'], // Loaders to handle CSS
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i, // Rule for image files
+        type: 'asset/resource', // Use Webpack's asset/resource for image handling
+        generator: {
+          filename: 'images/[name][ext]', // Output image folder
+        },
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/i, // Optimize images
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
