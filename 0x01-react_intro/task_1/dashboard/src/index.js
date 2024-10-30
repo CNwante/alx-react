765dbbe21ccca726c5dfb16ed019ce80802d3f21
Solomon Nwante <myphilothea@gmail.com>
2024-10-31 00:01:42 +0100

    Render notifications component

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -7,6 +7,7 @@ import Notifications from './Notifications';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Notifications />
     <App />
   </React.StrictMode>
 );
