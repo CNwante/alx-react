409088dbc602853d8b8da225d87dd2474fef2a9b
Solomon Nwante <myphilothea@gmail.com>
2024-10-31 07:55:58 +0100

    Add missing task requirements

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import './Notifications.css';
+import React from "react";
+import "./Notifications.css";
+import { getLatestNotification } from "./utils";
+import closeIcon from "./close-icon.png";
 
 const Notifications = () => {
-  return <div className="Notifications">Here is the list of notifications</div>;
-}
+  return (
+    <div className="Notifications">
+      <button
+        className="Close-btn"
+        aria-label="Close"
+        style={{ position: "absolute", right: "0", top: "5px" }}
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img src={closeIcon} className="Close-icon" alt="close" />
+      </button>
+
+      <p>Here is the list of notifications</p>
+
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
+    </div>
+  );
+};
 
 export default Notifications;
