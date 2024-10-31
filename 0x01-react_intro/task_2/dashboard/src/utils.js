06984b04016042f7e4bdafde34f140a7c17a94cc
Solomon Nwante <myphilothea@gmail.com>
2024-10-31 06:14:32 +0100

    Add task 3 files

---
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,9 @@
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+
+export function getFooterCopy(isIndex) {
+  return isIndex === true ?
+    'Holberton School' : 'Holberton School main dashboard';
+}
