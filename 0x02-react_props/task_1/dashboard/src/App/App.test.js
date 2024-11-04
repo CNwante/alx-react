8b4b9cfc21c7f55307aea03c78751a1fc94b0cf6
Solomon Nwante <myphilothea@gmail.com>
2024-11-04 23:50:59 +0100

    Add app component files

---
+++ 0x02-react_props/task_1/dashboard/src/App/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import App from './App';
+
+describe('App component', () => {
+  test('App renders without crashing', () => {
+    render(<App />);
+  });
+});
