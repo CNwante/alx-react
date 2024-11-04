f34ec4e219653f74e7fc1c2fc320ef3c240956aa
Solomon Nwante <myphilothea@gmail.com>
2024-11-04 22:48:01 +0100

    Remove root notifications

--- 0x02-react_props/task_0/dashboard/src/index.js
+++ 0x02-react_props/task_0/dashboard/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-import Notifications from './Notifications/Notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Notifications />
     <App />
   </React.StrictMode>
 );
