b891914b79e2a6018c42cfbab71f3a00f2caab43
Solomon Nwante <myphilothea@gmail.com>
2024-11-04 23:10:22 +0100

    Update test to suit changes

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -6,22 +6,4 @@ describe('App component', () => {
   test('App renders without crashing', () => {
     render(<App />);
   });
-
-  test('renders a div with the class App-header', () => {
-    render(<App />);
-    const headerElement = screen.getByRole('banner');
-    expect(headerElement).toHaveClass('App-header');
-  });
-
-  test('renders a div with the class App-body', () => {
-    render(<App />);
-    const bodyElement = screen.getByRole('main');
-    expect(bodyElement).toHaveClass('App-body');
-  });
-
-  test('renders a div with the class App-footer', () => {
-    render(<App />);
-    const footerElement = screen.getByRole('contentinfo');
-    expect(footerElement).toHaveClass('App-footer');
-  });
 });
