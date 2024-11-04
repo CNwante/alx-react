222278e6397d68883523da6f65e4f48aa77df425
Solomon Nwante <myphilothea@gmail.com>
2024-11-04 21:50:33 +0100

    Add notifications file

---
+++ 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Notifications.css";
+import { getLatestNotification } from "../utils/utils";
+import closeIcon from "../assets/close-icon.png";
+
+const Notifications = () => {
+  return (
+    <div className="Notifications">
+      <button
+        className="Close-btn"
+        aria-label="Close"
+        style={{ position: "absolute", right: "0", top: "5px" }}
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img src={closeIcon} className="Close-icon" alt="close" />
+      </button>
+
+      <p>Here is the list of notifications</p>
+
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Notifications;
