71725853f2ef8718067fa7158cea03f9bc0cf43e
Solomon Nwante <myphilothea@gmail.com>
2024-11-04 22:20:33 +0100

    Format code to improve readability

--- 0x02-react_props/task_0/dashboard/src/Header/Header.js
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -1,13 +1,13 @@
-import logo from '../assets/holberton-logo.png';
-import './Header.css';
+import logo from "../assets/holberton-logo.png";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header className="App-header">
-        <img src={logo} className="App-logo" alt="holberton-logo" />
-        <h1 className='App-title'>School dashboard</h1>
-      </header>
+      <img src={logo} className="App-logo" alt="holberton-logo" />
+      <h1 className="App-title">School dashboard</h1>
+    </header>
   );
-}
+};
 
 export default Header;
