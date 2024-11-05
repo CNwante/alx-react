1abfe5d1a421ba9a38df0de7efe7ca298d82e51b
Solomon Nwante <myphilothea@gmail.com>
2024-11-05 13:28:39 +0100

    Write tests for Login component

--- 0x02-react_props/task_1/dashboard/src/Login/Login.test.js
+++ 0x02-react_props/task_1/dashboard/src/Login/Login.test.js
@@ -2,9 +2,26 @@ import Login from "./Login";
 import { render, screen } from "@testing-library/react";
 
 describe("Login component", () => {
-  test("renders a div with the class Login-body", () => {
+  beforeEach(() => {
     render(<Login />);
-    const bodyElement = screen.getByRole("main");
-    expect(bodyElement).toHaveClass("Login-body");
+  });
+
+  test('renders Login component without crashing', () => {
+    const mainElement = screen.getByRole("main");
+    expect(mainElement).toHaveClass("Login-body");
+  });
+
+  test('renders 2 input fields and 2 label tags in Login component', () => {
+    const emailLabel = screen.getByLabelText(/email/i);
+    const passwordLabel = screen.getByLabelText(/password/i);
+
+    expect(emailLabel).toBeInTheDocument();
+    expect(passwordLabel).toBeInTheDocument();
+
+    const emailInput = screen.getByRole('textbox', {name: /email/i});
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
   });
 });
