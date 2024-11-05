7f20e7bcb83952851ffcf933a285a439670e8991
Solomon Nwante <myphilothea@gmail.com>
2024-11-05 13:27:41 +0100

    Write tests for Footer component

--- 0x02-react_props/task_1/dashboard/src/Footer/Footer.test.js
+++ 0x02-react_props/task_1/dashboard/src/Footer/Footer.test.js
@@ -2,9 +2,17 @@ import Footer from "./Footer";
 import {render, screen} from '@testing-library/react';
 
 describe('Footer component', () => {
+  beforeEach(() => {
+    render (<Footer />);
+  });
+
   test('renders a div with the class App-footer', () => {
-    render(<Footer />);
     const footerElement = screen.getByRole('contentinfo');
     expect(footerElement).toHaveClass('App-footer');
   });
+
+  test('renders the text “Copyright”', () => {
+    const copyrightText = screen.getByText(/Copyright/);
+    expect(copyrightText).toBeInTheDocument();
+  })
 });
