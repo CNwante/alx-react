f00c3936d69969e5a114bdbd4e9094c8559dc97c
Solomon Nwante <myphilothea@gmail.com>
2024-11-05 13:27:10 +0100

    Write tests for App component

--- 0x02-react_props/task_1/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_1/dashboard/src/App/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-  test('App renders without crashing', () => {
+  beforeEach(() => {
+    // Render the App component before each test
     render(<App />);
   });
+
+  test('renders without crashing', () => {
+    const divAppElement = screen.getByTestId('App');
+    expect(divAppElement).toBeInTheDocument();
+  });
+
+  test('contains the Notifications component', () => {
+    const Notifications = screen.getByTestId('Notifications');
+    expect(Notifications).toBeInTheDocument();
+  });
+
+  test('contains the Header component', () => {
+    const Header = screen.getByRole('heading');
+    expect(Header).toBeInTheDocument();
+  });
+
+  test('contains the Login component', () => {
+    const Login = screen.getByRole('main');
+    expect(Login).toBeInTheDocument();
+  });
+
+  test('contains the Footer component', () => {
+    const Footer = screen.getByRole('contentinfo');
+    expect(Footer).toBeInTheDocument();
+  })
 });
