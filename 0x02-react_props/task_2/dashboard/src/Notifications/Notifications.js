420f0aa7e1f75d3e7ea00c60152de539d73ae84f
Solomon Nwante <myphilothea@gmail.com>
2024-11-06 12:59:35 +0100

    Import and use NotificationItem component

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Notifications.css";
 import { getLatestNotification } from "../utils/utils";
 import closeIcon from "../assets/close-icon.png";
+import NotificationItem from "./NotificationItem";
 
 const Notifications = () => {
   return (
@@ -18,9 +19,9 @@ const Notifications = () => {
       <p>Here is the list of notifications</p>
 
       <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent">New resume available</li>
-        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        <NotificationItem type="default" value="New course available"/>
+        <NotificationItem type="urgent" value="New resume available"/>
+        <NotificationItem type="urgent" html={getLatestNotification()}/>
       </ul>
     </div>
   );
