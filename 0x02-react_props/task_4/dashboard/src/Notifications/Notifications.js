a985439fdfef6dfa9f1c35eb2a0e5d241a949765
Solomon Nwante <myphilothea@gmail.com>
2024-11-07 05:24:03 +0100

    Add menu item div

--- 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
@@ -6,24 +6,27 @@ import NotificationItem from "./NotificationItem";
 
 const Notifications = () => {
   return (
-    <div className="Notifications" data-testid="Notifications">
-      <button
-        className="Close-btn"
-        aria-label="Close"
-        style={{ position: "absolute", right: "0", top: "5px" }}
-        onClick={() => console.log("Close button has been clicked")}
-      >
-        <img src={closeIcon} className="Close-icon" alt="close" />
-      </button>
+    <React.Fragment>
+      <div className="menuItem">Your notifications</div>
+      <div className="Notifications" data-testid="Notifications">
+        <button
+          className="Close-btn"
+          aria-label="Close"
+          style={{ position: "absolute", right: "0", top: "5px" }}
+          onClick={() => console.log("Close button has been clicked")}
+        >
+          <img src={closeIcon} className="Close-icon" alt="close" />
+        </button>
 
-      <p>Here is the list of notifications</p>
+        <p>Here is the list of notifications</p>
 
-      <ul>
-        <NotificationItem type="default" value="New course available"/>
-        <NotificationItem type="urgent" value="New resume available"/>
-        <NotificationItem type="urgent" html={getLatestNotification()}/>
-      </ul>
-    </div>
+        <ul>
+          <NotificationItem type="default" value="New course available" />
+          <NotificationItem type="urgent" value="New resume available" />
+          <NotificationItem type="urgent" html={getLatestNotification()} />
+        </ul>
+      </div>
+    </React.Fragment>
   );
 };
 
