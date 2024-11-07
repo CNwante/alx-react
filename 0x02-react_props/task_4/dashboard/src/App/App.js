fe833362ebadeeee4b24bb5000bbecc1572eb121
Solomon Nwante <myphilothea@gmail.com>
2024-11-07 05:04:26 +0100

    Conditionally render login form or table

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -1,21 +1,28 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
 import "./App.css";
 
-const App = () => {
+const App = ({ isLoggedIn = false }) => {
   return (
     <React.Fragment>
       <Notifications />
       <div className="App" data-testid="App">
         <Header />
-        <Login />
+        {isLoggedIn ? <CourseList /> : <Login />}
         <Footer />
       </div>
     </React.Fragment>
   );
+};
+
+// PropTypes for validation
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
 };
 
 export default App;
