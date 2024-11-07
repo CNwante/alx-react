da36fb715c954af9f092ffde085ee98d81072b8b
Solomon Nwante <myphilothea@gmail.com>
2024-11-07 23:34:39 +0100

    Add app component files

---
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import "./App.css";
+
+const App = ({ isLoggedIn = false }) => {
+  return (
+    <React.Fragment>
+      <div className="App" data-testid="App">
+        <div className="appContainer">
+          <Notifications displayDrawer={true} />
+          <Header />
+        </div>
+        {isLoggedIn ? <CourseList /> : <Login />}
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+};
+
+// PropTypes for validation
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+export default App;
