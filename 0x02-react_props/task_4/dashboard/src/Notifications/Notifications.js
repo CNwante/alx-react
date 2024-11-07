42ddf69ede7aebf6330c5f18fab8c31c0821d7ed
Solomon Nwante <myphilothea@gmail.com>
2024-11-07 09:52:28 +0100

    Add notification container div element

--- 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
@@ -8,29 +8,31 @@ import NotificationItem from "./NotificationItem";
 const Notifications = ({ displayDrawer = true }) => {
   return (
     <React.Fragment>
-      <div className="menuItem">
-        <p>Your notifications</p>
-      </div>
-      {displayDrawer && (
-        <div className="Notifications" data-testid="Notifications">
-          <button
-            className="Close-btn"
-            aria-label="Close"
-            style={{ position: "absolute", right: "0", top: "5px" }}
-            onClick={() => console.log("Close button has been clicked")}
-          >
-            <img src={closeIcon} className="Close-icon" alt="close" />
-          </button>
-
-          <p>Here is the list of notifications</p>
-
-          <ul>
-            <NotificationItem type="default" value="New course available" />
-            <NotificationItem type="urgent" value="New resume available" />
-            <NotificationItem type="urgent" html={getLatestNotification()} />
-          </ul>
+      <div className="notificationContainer">
+        <div className="menuItem">
+          <p>Your notifications</p>
         </div>
-      )}
+        {displayDrawer && (
+          <div className="Notifications" data-testid="Notifications">
+            <button
+              className="Close-btn"
+              aria-label="Close"
+              style={{ position: "absolute", right: "0", top: "5px" }}
+              onClick={() => console.log("Close button has been clicked")}
+            >
+              <img src={closeIcon} className="Close-icon" alt="close" />
+            </button>
+
+            <p>Here is the list of notifications</p>
+
+            <ul>
+              <NotificationItem type="default" value="New course available" />
+              <NotificationItem type="urgent" value="New resume available" />
+              <NotificationItem type="urgent" html={getLatestNotification()} />
+            </ul>
+          </div>
+        )}
+      </div>
     </React.Fragment>
   );
 };
