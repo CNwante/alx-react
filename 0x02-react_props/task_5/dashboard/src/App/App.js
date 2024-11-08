a093c8c2be1c4237af7f37f6a32e100929a2f917
Solomon Nwante <myphilothea@gmail.com>
2024-11-08 06:25:05 +0100

    Add courses nad notifications lists

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -5,17 +5,30 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Login from "../Login/Login";
 import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
 import "./App.css";
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
 
 const App = ({ isLoggedIn = false }) => {
   return (
     <React.Fragment>
       <div className="App" data-testid="App">
         <div className="appContainer">
-          <Notifications displayDrawer={true} />
+          <Notifications listNotifications={listNotifications} displayDrawer={true} />
           <Header />
         </div>
-        {isLoggedIn ? <CourseList /> : <Login />}
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
         <Footer />
       </div>
     </React.Fragment>
