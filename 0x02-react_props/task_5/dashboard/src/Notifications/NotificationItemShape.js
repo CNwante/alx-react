f3eec8f614881592c8d536aceadea74aab66b3b1
Solomon Nwante <myphilothea@gmail.com>
2024-11-08 04:44:15 +0100

    Add shape for notification item component

---
+++ 0x02-react_props/task_5/dashboard/src/Notifications/NotificationItemShape.js
@@ -0,0 +1,8 @@
+const NotificationItemShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  html: PropTypes.shape({ __html: PropTypes.string }),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+});
+
+export default NotificationItemShape;
