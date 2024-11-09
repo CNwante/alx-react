d9dd7da2871d1a6214165cda6dd9c708ccc92a27
Solomon Nwante <myphilothea@gmail.com>
2024-11-09 14:13:31 +0100

    Import prop types

--- 0x02-react_props/task_5/dashboard/src/Notifications/NotificationItemShape.js
+++ 0x02-react_props/task_5/dashboard/src/Notifications/NotificationItemShape.js
@@ -1,4 +1,6 @@
-const NotificationItemShape = propTypes.shape({
+import PropTypes from 'prop-types';
+
+const NotificationItemShape = PropTypes.shape({
   id: PropTypes.number.isRequired,
   html: PropTypes.shape({ __html: PropTypes.string }),
   type: PropTypes.string.isRequired,
