dd61b6e3e4f25291d12de6f722c6a0379940ee8b
Solomon Nwante <myphilothea@gmail.com>
2024-11-09 14:51:17 +0100

    Wrap test case in a test function

--- 0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.test.js
@@ -2,18 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CourseListRow from './CourseListRow';
 
-
 describe('CourseListRow component', () => {
-  test('rensers a single header cell with colspan=2 isHeader=true textSecondCell=null', () => {
+  test('renders a single header cell with colspan=2 when isHeader is true and textSecondCell is null', () => {
     render(<CourseListRow isHeader={true} textFirstCell="Header Text" />);
+
+    const thElement = screen.getByRole('columnheader');
+    expect(thElement).toBeInTheDocument();
+    expect(thElement).toHaveAttribute('colSpan', '2');
   });
 
-
-  const thElement = screen.getByRole('columnheader');
-  expect(thElement).toBeInTheDocument();
-  expect(thElement).toHaveAttribute('colSpan', '2');
-
-  test('renders two cells when textSecondCell is present', () => {
+  test('renders two header cells when textSecondCell is present', () => {
     render(<CourseListRow isHeader={true} textFirstCell="Header 1" textSecondCell="Header 2" />);
 
     const thElements = screen.getAllByRole('columnheader');
@@ -23,7 +21,7 @@ describe('CourseListRow component', () => {
   test('renders two data cells when isHeader is false', () => {
     render(<CourseListRow isHeader={false} textFirstCell="Cell 1" textSecondCell="Cell 2" />);
 
-    const tdElement = screen.getAllByRole('cell');
-    expect(tdElement).toHaveLength(2);
+    const tdElements = screen.getAllByRole('cell');
+    expect(tdElements).toHaveLength(2);
   });
 });
