78094641d3dbd201979fa7d4e54bd61fa315c42a
Solomon Nwante <myphilothea@gmail.com>
2024-11-12 16:10:25 +0100

    Modify footer component to use aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import {getFullYear, getFooterCopy} from '../utils/utils';
 
 const Footer = () => {
   return (
-    <footer className="App-footer ">
+    <footer className={css(styles.appFooter)}>
       Copyright {getFullYear()} - {getFooterCopy(true)}
     </footer>
   );
 };
+
+const styles = StyleSheet.create({
+  appFooter: {
+    padding: "1em 0",
+    textAlign: "center",
+    fontStyle: "italic",
+    borderTop: "3px solid rgb(194, 79, 79)",
+  },
+});
 
 export default Footer;
