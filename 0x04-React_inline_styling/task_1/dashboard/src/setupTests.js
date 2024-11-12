c3b9d152e97b5e33d55110c4f23e3281d9c9bbd7
Solomon Nwante <myphilothea@gmail.com>
2024-11-12 15:45:27 +0100

    Mock aphrodite in tests
    To resolve the type error isssue that occured after installing aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/setupTests.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock aphrodite in tests to resolve the type error isssue:
+// TypeError: Cannot read properties of null (reading 'querySelector')
+jest.mock('aphrodite', () => {
+  const mockStyleSheet = {
+    create: () => ({}),
+    css: () => "",
+  };
+  return { StyleSheet: mockStyleSheet, css: mockStyleSheet.css };
+});
