fb12e24edd5f1f2466cd399c023a7b2c3a84fe85
Solomon Nwante <myphilothea@gmail.com>
2024-11-12 18:37:28 +0100

    Modify component to use aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -1,8 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { StyleSheet, css } from 'aphrodite';
 
 const headerStyle = { backgroundColor: "#deb5b545" };
 const rowStyle = { backgroundColor: "#f5f5f5ab" };
+
+const styles = StyleSheet.create({
+
+  header: {
+    borderBottom: '1px solid #ddd',
+    textAlign: 'left',
+  },
+});
 
 function CourseListRow({ isHeader = false, textFirstCell, textSecondCell = null }) {
   return (
@@ -10,16 +19,16 @@ function CourseListRow({ isHeader = false, textFirstCell, textSecondCell = null 
       {isHeader ? (
         textSecondCell ? (
           <>
-            <th>{textFirstCell}</th>
-            <th>{textSecondCell}</th>
+            <th className={css(styles.header)}>{textFirstCell}</th>
+            <th className={css(styles.header)}>{textSecondCell}</th>
           </>
         ) : (
           <th colSpan="2">{textFirstCell}</th>
         )
       ) : (
         <>
-          <td>{textFirstCell}</td>
-          <td>{textSecondCell}</td>
+          <td className={css(styles.header)}>{textFirstCell}</td>
+          <td className={css(styles.header)}>{textSecondCell}</td>
         </>
       )}
     </tr>
