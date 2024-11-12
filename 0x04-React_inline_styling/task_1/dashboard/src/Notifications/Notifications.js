b2904b4107859bdd16e4bd4b4594f860158829c1
Solomon Nwante <myphilothea@gmail.com>
2024-11-12 19:28:36 +0100

    Modify notification component to use aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "./Notifications.css";
+import { StyleSheet, css } from "aphrodite";
 import closeIcon from "../assets/close-icon.png";
 import NotificationItem from "./NotificationItem";
 import NotificationItemShape from "./NotificationItemShape";
@@ -21,7 +21,6 @@ class Notifications extends Component {
     listNotifications: [],
   };
 
-  // Only update when listNotifications has more items than before
   shouldComponentUpdate(nextProps) {
     return nextProps.listNotifications.length > this.props.listNotifications.length;
   }
@@ -34,46 +33,68 @@ class Notifications extends Component {
     const { displayDrawer, listNotifications } = this.props;
 
     return (
-      <React.Fragment>
-        <div className="notificationContainer">
-          <div className="menuItem">
-            <p>Your notifications</p>
-          </div>
-          {displayDrawer && (
-            <div className="Notifications" data-testid="Notifications">
-              <button
-                className="Close-btn"
-                aria-label="Close"
-                style={{ position: "absolute", right: "0", top: "5px" }}
-                onClick={() => console.log("Close button has been clicked")}
-              >
-                <img src={closeIcon} className="Close-icon" alt="close" />
-              </button>
-
-              <p>Here is the list of notifications</p>
-
-              <ul>
-                {listNotifications.length === 0 ? (
-                  <li>No new notification for now</li>
-                ) : (
-                  listNotifications.map((notification) => (
-                    <NotificationItem
-                      key={notification.id}
-                      id={notification.id}
-                      type={notification.type}
-                      value={notification.value}
-                      html={notification.html}
-                      markAsRead={this.markAsRead}
-                    />
-                  ))
-                )}
-              </ul>
-            </div>
-          )}
+      <div className={css(styles.notificationContainer)}>
+        <div className={css(styles.menuItem)}>
+          <p>Your notifications</p>
         </div>
-      </React.Fragment>
+        {displayDrawer && (
+          <div className={css(styles.notifications)} data-testid="Notifications">
+            <button
+              className={css(styles.closeBtn)}
+              aria-label="Close"
+              onClick={() => console.log("Close button has been clicked")}
+            >
+              <img src={closeIcon} className={css(styles.closeIcon)} alt="close" />
+            </button>
+
+            <p>Here is the list of notifications</p>
+
+            <ul>
+              {listNotifications.length === 0 ? (
+                <li>No new notification for now</li>
+              ) : (
+                listNotifications.map((notification) => (
+                  <NotificationItem
+                    key={notification.id}
+                    id={notification.id}
+                    type={notification.type}
+                    value={notification.value}
+                    html={notification.html}
+                    markAsRead={this.markAsRead}
+                  />
+                ))
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  notificationContainer: {
+    position: "relative",
+  },
+  menuItem: {
+    marginBottom: "1em",
+  },
+  notifications: {
+    position: "relative",
+    border: "2px dashed #c24f4f",
+    padding: "2em 0",
+  },
+  closeBtn: {
+    border: "none",
+    background: "transparent",
+    cursor: "pointer",
+    position: "absolute",
+    right: "0",
+    top: "5px",
+  },
+  closeIcon: {
+    width: "20px",
+  },
+});
 
 export default Notifications;
