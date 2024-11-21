195ed87939e0e7b19097aafcec500c5d9a8a4d1b
Solomon Nwante <myphilothea@gmail.com>
2024-11-21 10:16:44 +0100

    Move test code to test folder

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,14 +3,3 @@ import { fromJS } from "immutable";
 export default function getImmutableObject(object) {
   return fromJS(object)
 }
-
-// Example usage:
-const sampleObject = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-const immutableMap = getImmutableObject(sampleObject);
-console.log(immutableMap);
