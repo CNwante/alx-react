54a15945226133b1e0c64e2b5014c617c481c59e
Solomon Nwante <myphilothea@gmail.com>
2024-11-26 10:51:19 +0100

    Filter normalized schema

--- 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -1,6 +1,7 @@
 import notificationsData from "../../../../notifications.json";
 import { normalize, schema } from "normalizr";
 
+// Define schemas
 const user = new schema.Entity("users");
 const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
 const notification = new schema.Entity("notifications", {
@@ -8,12 +9,22 @@ const notification = new schema.Entity("notifications", {
   context: message,
 });
 
+// Normalize data
 const normalizedData = normalize(notificationsData, [notification]);
 
+// Function using normalized data
 function getAllNotificationsByUser(userId) {
-  return notificationsData
-    .filter((notification) => notification.author.id === userId)
-    .map((notification) => notification.context);
+  const notifications = normalizedData.entities.notifications;
+  const messages = normalizedData.entities.messages;
+
+  const userNotifications = [];
+  for (const id in notifications) {
+    if (notifications[id].author === userId) {
+      userNotifications.push(messages[notifications[id].context]);
+    }
+  }
+
+  return userNotifications;
 }
 
 export { normalizedData, getAllNotificationsByUser };
