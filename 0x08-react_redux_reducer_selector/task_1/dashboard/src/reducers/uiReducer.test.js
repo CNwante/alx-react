d2d8950576035efcbff764640aff601fe572d488
Solomon Nwante <myphilothea@gmail.com>
2024-12-03 12:38:06 +0100

    Update tests to fit the new changes

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -4,67 +4,45 @@ import * as uiActions from "../actions/uiActionTypes";
 describe("uiReducer", () => {
   it("should return the initial state when no action is passed", () => {
     const newState = uiReducer(undefined, {});
-    expect(newState).toEqual(initialState);
+    expect(newState.toJS()).toEqual(initialState.toJS());
   });
 
-  it("should return the initial state when an unrelated action is passed", () => {
+  it("should return the initial state when a wrong action is passed", () => {
     const newState = uiReducer(initialState, { type: "SELECT_COURSE" });
-    expect(newState).toEqual(initialState);
+    expect(newState.toJS()).toEqual(initialState.toJS());
   });
 
   it("should set isNotificationDrawerVisible to true when DISPLAY_NOTIFICATION_DRAWER is passed", () => {
-    const state = {
-      ...initialState,
-      isNotificationDrawerVisible: true,
-    }
-
     const newState = uiReducer(initialState, {
       type: uiActions.DISPLAY_NOTIFICATION_DRAWER,
     });
-    expect(newState).toEqual(state);
+
+    expect(newState).toEqual(initialState.set("isNotificationDrawerVisible", true));
   });
 
   it("should set isNotificationDrawerVisible to false when HIDE_NOTIFICATION_DRAWER is passed", () => {
-    const prevState = {
-      ...initialState,
-      isNotificationDrawerVisible: true,
-    };
+    const prevState = initialState.set("isNotificationDrawerVisible", true);
 
     const newState = uiReducer(prevState, {
       type: uiActions.HIDE_NOTIFICATION_DRAWER,
     });
 
-    expect(newState).toEqual({
-      ...prevState,
-      isNotificationDrawerVisible: false,
-    });
+    expect(newState).toEqual(initialState.set("isNotificationDrawerVisible", false));
   });
 
   it("should set isUserLoggedIn to true when LOGIN_SUCCESS is passed", () => {
     const newState = uiReducer(initialState, { type: uiActions.LOGIN_SUCCESS });
-    expect(newState).toEqual({
-      ...initialState,
-      isUserLoggedIn: true,
-    });
+    expect(newState).toEqual(initialState.set("isUserLoggedIn", true));
   });
 
   it("should set isUserLoggedIn to false when LOGIN_FAILURE is passed", () => {
     const newState = uiReducer(initialState, { type: uiActions.LOGIN_FAILURE });
-    expect(newState).toEqual({
-      ...initialState,
-      isUserLoggedIn: false,
-    });
+    expect(newState).toEqual(initialState.set("isUserLoggedIn", false));
   });
 
   it("should set isUserLoggedIn to false when LOGOUT is passed", () => {
-    const prevState = {
-      ...initialState,
-      isUserLoggedIn: true,
-    };
+    const prevState = initialState.set("isUserLoggedIn", true);
     const newState = uiReducer(prevState, { type: uiActions.LOGOUT });
-    expect(newState).toEqual({
-      ...prevState,
-      isUserLoggedIn: false,
-    });
+    expect(newState).toEqual(initialState.set("isUserLoggedIn", false));
   });
 });
