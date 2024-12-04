9cf0769cb14d2baa72b89bfcd1d6f423d4661afb
Solomon Nwante <myphilothea@gmail.com>
2024-12-04 18:59:54 +0100

    Update notifications reducer tests

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.test.js
@@ -4,6 +4,7 @@ import {
   SET_TYPE_FILTER,
 } from "../actions/notificationActionTypes";
 import { notificationReducer, defaultState } from "./notificationReducer";
+import { fromJS } from "immutable";
 
 describe("notificationReducer", () => {
   it("should return the default state", () => {
@@ -18,41 +19,50 @@ describe("notificationReducer", () => {
         { id: 2, type: "urgent", value: "New resume available" },
       ],
     };
-    const expectedState = {
-      filter: "DEFAULT",
-      notifications: [
-        { id: 1, type: "default", value: "New course available", isRead: false },
-        { id: 2, type: "urgent", value: "New resume available", isRead: false },
-      ],
-    };
+
+    const expectedState = defaultState
+      .mergeDeep(
+        fromJS({
+          entities: {
+            notifications: {
+              1: { id: 1, type: "default", value: "New course available", isRead: false },
+              2: { id: 2, type: "urgent", value: "New resume available", isRead: false },
+            },
+          },
+          result: [1, 2],
+        })
+      );
+
     expect(notificationReducer(undefined, action)).toEqual(expectedState);
   });
 
   it("should handle MARK_AS_READ", () => {
-    const initialState = {
-      filter: "DEFAULT",
-      notifications: [
-        { id: 1, type: "default", value: "New course available", isRead: false },
-        { id: 2, type: "urgent", value: "New resume available", isRead: false },
-      ],
-    };
+    const initialState = defaultState.mergeDeep(
+      fromJS({
+        entities: {
+          notifications: {
+            1: { id: 1, type: "default", value: "New course available", isRead: false },
+            2: { id: 2, type: "urgent", value: "New resume available", isRead: false },
+          },
+        },
+        result: [1, 2],
+      })
+    );
+
     const action = { type: MARK_AS_READ, index: 2 };
-    const expectedState = {
-      ...initialState,
-      notifications: [
-        { id: 1, type: "default", value: "New course available", isRead: false },
-        { id: 2, type: "urgent", value: "New resume available", isRead: true },
-      ],
-    };
+
+    const expectedState = initialState.setIn(
+      ["entities", "notifications", "2", "isRead"],
+      true
+    );
+
     expect(notificationReducer(initialState, action)).toEqual(expectedState);
   });
 
   it("should handle SET_TYPE_FILTER", () => {
     const action = { type: SET_TYPE_FILTER, filter: "URGENT" };
-    const expectedState = {
-      ...defaultState,
-      filter: "URGENT",
-    };
+    const expectedState = defaultState.set("filter", "URGENT");
+
     expect(notificationReducer(undefined, action)).toEqual(expectedState);
   });
 });
