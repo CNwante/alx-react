97c965940c07bd6cf6b244642d2d745116c95807
Solomon Nwante <myphilothea@gmail.com>
2024-12-04 18:59:25 +0100

    Refactor notification reducer with immutable

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,39 +1,49 @@
+import { fromJS } from "immutable";
 import {
+  FETCH_NOTIFICATIONS_SUCCESS,
   MARK_AS_READ,
   SET_TYPE_FILTER,
-  FETCH_NOTIFICATIONS_SUCCESS,
 } from "../actions/notificationActionTypes";
+import { notificationsNormalizer } from "../schema/notifications";
 
-export const defaultState = {
-  notifications: [],
+export const defaultState = fromJS({
+  entities: {
+    notifications: {},
+    users: {},
+    messages: {},
+  },
+  result: [],
   filter: "DEFAULT",
-};
+});
 
 export const notificationReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case FETCH_NOTIFICATIONS_SUCCESS:
-      return {
-        ...state,
-        notifications: action.data.map((notification) => ({
-          ...notification,
-          isRead: false,
-        })),
-      };
-    case MARK_AS_READ:
-      return {
-        ...state,
-        notifications: state.notifications.map((notification) =>
-          notification.id === action.index
-            ? { ...notification, isRead: true }
-            : notification
-        ),
-      };
+    case FETCH_NOTIFICATIONS_SUCCESS: {
+      const normalizedData = notificationsNormalizer(action.data);
+      return state.mergeDeep(fromJS(normalizedData)).setIn(
+        ["entities", "notifications"],
+        fromJS(
+          Object.entries(normalizedData.entities.notifications).reduce(
+            (acc, [key, value]) => ({
+              ...acc,
+              [key]: { ...value, isRead: false },
+            }),
+            {}
+          )
+        )
+      );
+    }
 
-    case SET_TYPE_FILTER:
-      return {
-        ...state,
-        filter: action.filter,
-      };
+    case SET_TYPE_FILTER: {
+      return state.set("filter", action.filter);
+    }
+
+    case MARK_AS_READ: {
+      return state.setIn(
+        ["entities", "notifications", action.index.toString(), "isRead"],
+        true
+      );
+    }
 
     default:
       return state;
