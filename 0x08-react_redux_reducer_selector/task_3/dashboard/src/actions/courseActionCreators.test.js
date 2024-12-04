4fea8122439608b0a2eabc7cf447473d897b064e
Solomon Nwante <myphilothea@gmail.com>
2024-12-04 06:50:10 +0100

    Refactor test to improve readability

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/courseActionCreators.test.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -10,22 +10,22 @@ describe("Test for action creators", () => {
     expect(unSelectCourse(1)).toEqual({ type: UNSELECT_COURSE, index: 1 });
   });
 
-  test("fetchCourse success returns the correct data", () => {
-    const expectedResult = {
-      type: FETCH_COURSE_SUCCESS,
-      data: [
-        { id: 1, name: "ES6", isSelected: false, credit: 60 },
-        { id: 2, name: "Webpack", isSelected: false, credit: 20 },
-        { id: 3, name: "React", isSelected: false, credit: 40 },
-      ]
-    };
-
-    const data = [
-      { id: 1, name: "ES6", isSelected: false, credit: 60 },
-      { id: 2, name: "Webpack", isSelected: false, credit: 20 },
-      { id: 3, name: "React", isSelected: false, credit: 40 },
+  test("fetchCourseSuccess returns the correct course data with isSelected: false", () => {
+    const courses = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
     ];
 
-    expect(fetchCourseSuccess(data)).toEqual(expectedResult);
+    const expectedResult = {
+      type: "FETCH_COURSE_SUCCESS",
+      data: [
+        { id: 1, name: "ES6", credit: 60, isSelected: false },
+        { id: 2, name: "Webpack", credit: 20, isSelected: false },
+        { id: 3, name: "React", credit: 40, isSelected: false },
+      ],
+    };
+
+    expect(fetchCourseSuccess(courses)).toEqual(expectedResult);
   });
 });
