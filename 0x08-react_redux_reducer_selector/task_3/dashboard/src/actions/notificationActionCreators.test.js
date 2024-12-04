69f0a5d41599fb5a23a9cd3e86b4877d98b93057
Solomon Nwante <myphilothea@gmail.com>
2024-12-04 06:31:47 +0100

    Add fetch notifications success test

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/notificationActionCreators.test.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/notificationActionCreators.test.js
@@ -1,8 +1,54 @@
-import { NotificationTypeFilters } from "./notificationActionTypes";
-import { markAsRead, setNotificationFilter } from "./notificationActionCreators";
+import { NotificationTypeFilters, FETCH_NOTIFICATIONS_SUCCESS } from "./notificationActionTypes";
+import { fetchNotificationsSuccess, markAsRead, setNotificationFilter } from "./notificationActionCreators";
 
 describe("Test for Notifications Action Creators", () => {
-  test("Calling the creator with 1 as an argument should return obj", () => {
+  test("Calling fetchNotificationsSuccess returns the correct notification data", () => {
+    const notifications = [
+      {
+        id: 1,
+        type: "default",
+        value: "New course available"
+      },
+      {
+        id: 2,
+        type: "urgent",
+        value: "New resume available"
+      },
+      {
+        id: 3,
+        type: "urgent",
+        value: "New data available"
+      }
+    ];
+
+    const expectedResult = {
+      type: FETCH_NOTIFICATIONS_SUCCESS,
+      data: [
+        {
+          id: 1,
+          isRead: false,
+          type: "default",
+          value: "New course available"
+        },
+        {
+          id: 2,
+          isRead: false,
+          type: "urgent",
+          value: "New resume available"
+        },
+        {
+          id: 3,
+          isRead: false,
+          type: "urgent",
+          value: "New data available"
+        }
+      ]
+    };
+
+    expect(fetchNotificationsSuccess(notifications)).toEqual(expectedResult);
+  });
+
+  test("Calling markAsRead with 1 as an argument should return the correct object", () => {
     const expectedResult = {
       type: "MARK_AS_READ",
       index: 1,
@@ -10,7 +56,7 @@ describe("Test for Notifications Action Creators", () => {
     expect(markAsRead(1)).toEqual(expectedResult);
   });
 
-  test("Calling the creator with DEFAULT filter return the obj", () => {
+  test("Calling setNotificationFilter with DEFAULT filter returns the correct object", () => {
     const expectedResult = {
       type: "SET_TYPE_FILTER",
       filter: "DEFAULT",
