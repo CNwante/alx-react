8c8010e15854bed9c6f1e8be8de47985f02498d5
Solomon Nwante <myphilothea@gmail.com>
2024-12-04 20:29:14 +0100

    Add schema files

---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/schema/courses.js
@@ -0,0 +1,7 @@
+import { normalize, schema } from "normalizr";
+
+const course = new schema.Entity("courses");
+
+const coursesNormalizer = (data) => normalize(data, [course]);
+
+export { coursesNormalizer };
