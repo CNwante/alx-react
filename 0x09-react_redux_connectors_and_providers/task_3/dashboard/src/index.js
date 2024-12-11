aff2d60a34daad3d992cfc5a24ed0a8c98a9a6b5
Solomon Nwante <myphilothea@gmail.com>
2024-12-11 16:47:10 +0100

    Add index.js file

---
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { thunk } from "redux-thunk";
+import App from './App/App';
+import { uiReducer } from './reducers/uiReducer';
+import reportWebVitals from './reportWebVitals';
+
+// Create the Redux store with the uiReducer
+const store = createStore(uiReducer, applyMiddleware(thunk));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// Measuring performance
+reportWebVitals();
