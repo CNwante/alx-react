e3ebd70bd2acb3d630546b2d8d8705bd6c423323
Solomon Nwante <myphilothea@gmail.com>
2024-12-11 16:42:39 +0100

    Test connected component

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,26 +1,26 @@
 import Footer from "./Footer";
-import {render, screen} from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { uiReducer } from "../reducers/uiReducer";
 
 const store = createStore(uiReducer);
 
-describe('Footer component', () => {
+describe("Footer component", () => {
   beforeEach(() => {
-    render (
+    render(
       <Provider store={store}>
-            <Footer />
+        <Footer />
       </Provider>
-  );
+    );
   });
 
-  test('renders element with the role contentinfo', () => {
-    const footerElement = screen.getByRole('contentinfo');
+  test("renders element with the role contentinfo", () => {
+    const footerElement = screen.getByRole("contentinfo");
     expect(footerElement).toBeInTheDocument();
   });
 
-  test('renders the text “Copyright”', () => {
+  test("renders the text “Copyright”", () => {
     const copyrightText = screen.getByText(/Copyright/);
     expect(copyrightText).toBeInTheDocument();
   });
